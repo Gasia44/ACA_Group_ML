0911015c6b74159e967c1a97b4571eb05ee1625c
Gasia44 <gasiaatashian@gmail.com>
2017-05-14 02:22:58 +0400

    re_implemented read_csv

--- csv_utils.py
+++ csv_utils.py
@@ -1,56 +1,33 @@
 import pandas as pd
 
-def hasNumbers(inputString):
-    return all(char.isdigit() for char in inputString)
+def read_csv(file_to_reed, header=True, seperator = ','):
+    '''''
+    read csv file using specified seperator
+    take as argument the file that will be read, the header and the seperator;
+    if the header is None, then columns' label are 1,2,3,...
+    return a dataframe
+    '''''
 
-def read_csv(file_to_reed, header=None):
-    #file_to_reed = "SPECTF.dat"
     with open(file_to_reed) as f:           
-        data = f.readlines() 
-        #print(data)
-    temp=''   
-    flag = 0
-    if header != None:
-        flag = 1
-    df =pd.DataFrame( index=range(len(data)), columns=range(len(data[0]))) 
-    row=0
-    
-    for i in range(len(data)):
-        temp=''
-        col = 0
-        for j in range(len(data[i])):
-            if(data[i][j] !=','):
-                temp=temp + str(data[i][j])
-                tempfinal =temp
-            else:  
+        data = f.readlines()
 
-                if flag == 1 and i == 0:
-                    df = df.rename(columns = {str(col): temp})
-                    col = col + 1
-                    row =-1
-                    temp =''
-                  
-                    
-                else:
-                    if(hasNumbers(temp)):
-                        df.loc[row, col]= int(temp)
-                    else: 
-                        df.loc[row, col] = temp
-                    col = col+1
-                temp=''
-        
-        if(tempfinal[-1] =='\n'):
-            tempfinal = tempfinal[0:len(tempfinal)-1]
-        if(hasNumbers(tempfinal) and row !=-1):
-                df.loc[row, col]= int(tempfinal)
-        else: 
-            if(row !=-1):
-                df.loc[row, col] = tempfinal
-                col = col+1
-                temp=''
-        row = row +1
-        
-    df=df.dropna(axis=1, how='all')
-    df=df.dropna(axis=0, how='all')
+    strip_data = [a.strip() for a in data]
+
+    list_of_rows = []
+
+    # column label
+    label_data = (strip_data[0].split(seperator))
+
+    if header == None:
+        list_of_rows.append(label_data)
+
+    for i in range(1, len(data)):
+        one_row = (strip_data[i].split(seperator))
+        list_of_rows.append(one_row)
+
+    df =pd.DataFrame(list_of_rows)
+
+    if header != None:
+        df.columns = label_data
+
     return df
-    
